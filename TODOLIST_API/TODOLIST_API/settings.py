edb29ee00ad0c0612899fa9016eaef4e9334cf41
oka4dc <okaforchikelue93@gmail.com>
2024-07-27 07:11:39 +0100

    database migration and swagger UI error fix

--- TODOLIST_API/TODOLIST_API/settings.py
+++ TODOLIST_API/TODOLIST_API/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'API_App',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- TODOLIST_API/TODOLIST_API/urls.py
+++ TODOLIST_API/TODOLIST_API/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
       title="Todo API",
       default_version='v1',
       description="Simple To-Do List API",
-      terms_of_servicepip install drf-yasg="https://www.google.com/policies/terms/",
+      #terms_of_servicepip install drf-yasg="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@todos.local"),
       license=openapi.License(name="BSD License"),
    ),
@@ -37,7 +37,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todoapp.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('API_App.urls')),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+
